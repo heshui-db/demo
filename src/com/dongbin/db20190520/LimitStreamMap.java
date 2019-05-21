9a69b4a082c0a3ff4a2f9dc2c2e01a43017e215b
W23123 <1021720039@qq.com>
2019-05-21 11:09:50 +0800

    LimitStreamMap

---
+++ src/com/dongbin/db20190520/LimitStreamMap.java
@@ -0,0 +1,34 @@
+package com.dongbin.db20190520;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicLong;
+
+public class LimitStreamMap {
+    private final static Map<Long, AtomicLong> limitMap = new ConcurrentHashMap<>();
+
+    private int limit;
+
+    private long threshold;
+
+    public LimitStreamMap(int limit, long threshold, TimeUnit timeUnit) {
+        this.limit = limit;
+        this.threshold = threshold;
+    }
+
+    public LimitStreamMap() {
+        this(10, 1000000, TimeUnit.MINUTES);
+    }
+
+    public boolean isLimit() {
+        long current = System.currentTimeMillis() / 1000;
+        AtomicLong atomicLong = limitMap.get(current);
+        if (atomicLong == null) {
+            atomicLong = new AtomicLong();
+            limitMap.remove(current - 1);
+        }
+        return atomicLong.incrementAndGet() > limit;
+    }
+
+}
